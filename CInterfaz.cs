2235357e68c6ce8409c04765d4275bf60218dafc
Gerald <geraldjoly191@gmail.com>
2024-11-05 14:39:53 -0300

    Agregué La clase Interfaz del Proyecto

---
+++ CInterfaz.cs
@@ -0,0 +1,33 @@
+﻿using Spectre.Console;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.InteropServices.Marshalling;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUD_de_Personas
+{
+    public class CInterfaz
+    {
+
+        public string Menu()
+        {
+            Console.Clear();
+            string opcion = AnsiConsole.Prompt(
+        new SelectionPrompt<string>()
+        .Title("[green]Ingrese la Opcion elegida [/]?")
+        .PageSize(10)
+        .MoreChoicesText("[red](Usa las teclas de Direcion para elegir )[/]")
+        .AddChoices(new[] {
+            "Mostar la Lista de Personas","Agregar Una Nueva Persona","Eliminar Una Persona","Salir"
+        }));
+            
+            
+            return opcion;
+        }
+
+        
+    }
+}
