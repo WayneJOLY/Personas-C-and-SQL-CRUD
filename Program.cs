7f4efb3acbb05537421d74e0e819f48e164bc8fa
Gerald <geraldjoly191@gmail.com>
2024-11-05 11:04:40 -0300

    El punto 1 esa funcional

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using Spectre.Console;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace CRUD_de_Personas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string opcion;
+            CInterfaz interfaz = new CInterfaz();
+
+           interfaz.Menu();
+
+            //Console.Write(opcion);
+
+            do
+            {
+
+                opcion = interfaz.Menu();
+
+                switch(opcion)
+                {
+                    case "Mostar la Lista de Personas":
+
+                        var table = new Table();
+                        table.AddColumn("ID");
+                        table.AddColumn("Nombre");
+                        table.AddColumn("Apellido");
+                        List<CPersona> list = CPersonaAccesoDatos.Leer();
+                        foreach (CPersona persona in list)
+                        {
+                            table.AddRow(persona.Id.ToString(), persona.Nombre, persona.Apellido);
+                        }
+                        AnsiConsole.Render(table);
+                        break;
+                    case "Agregar Una Nueva Persona":
+                        break;
+                    case "Eliminar Una Persona":
+                        break;
+                }
+
+            } while (opcion != "Salir");
+
+            
+
+
+        }
+    }
+}
